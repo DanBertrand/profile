cae8bee3a85cff11951afa312783068bc10835e6
DanBertrand <dan.bertrand.hs@gmail.com>
2021-04-28 14:04:37 +0200

    test ssh

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import { useState} from 'react';
   
 const Home = () => {
+
+  const [input, setInput] = useState('');
   
 return (
 <div className='Home'>
   <h2>Home</h2>
+  <input type="text" value={input} onChange={e => setInput(e.target.value)} ></input>
+  <p>{input}</p>
 </div>
 )
 }
